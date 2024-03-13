0b137a921749990648c6e942261400e1ac0717a9
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2024-03-13 13:51:18 +0600

    🚀 Add: vite-tsconfig-paths package and configure paths in tsconfig.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import react from "@vitejs/plugin-react";
 import path from "node:path";
 import { defineConfig } from "vite";
 import electron from "vite-plugin-electron/simple";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tsconfigPaths(),
     electron({
       main: {
         // Shortcut of `build.lib.entry`.

dc041e206a6e1cf9131082782603b1b4bee669f8
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2024-03-13 12:41:01 +0600

    🧼 Update: ESLint and Prettier configurations

---
+++ .prettierrc.mjs
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+const config = {
+    printWidth: 80,
+    semi: true,
+    singleQuote: false,
+    tabWidth: 4,
+    trailingComma: "es5",
+    useTabs: false,
+    arrowParens: "always",
+    experimentalTernaries: true,
+};
+
+export default config;
